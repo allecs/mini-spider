c662c5ead95fc4de76b409385725521ac6b54d71
Lixiang Ao <aolixiang@baidu.com>
2014-08-06 22:26:21 +0800

    finish downloader and test cases

--- downloader.py
+++ downloader.py
@@ -16,18 +16,39 @@ class Downloader(object):
     下载器
     """
 
-    def __init__(self, thread_count, item_handler=None):
+    def __init__(self, thread_count, crawl_timeout, item_handler=None, scheduler=None):
         self._item_handler = item_handler
+        self._scheduler = scheduler
+        self._crawl_timeout = crawl_timeout
         self._in_queue = Queue.Queue()
         self._threads = []
+        self._init_threads(thread_count)
 
+    def set_handler(self, handler):
+        self._item_handler = handler
+
+    def set_scheduler(self, scheduler):
+        self._scheduler = scheduler
+
+    def add_item(self, item):
+        self._in_queue.put(item)
+
+    def start(self):
+        if not self._item_handler:
+            logging.critical('ItemHandler not found, exiting')
+            sys.exit(1)
+        if not self._scheduler:
+            logging.critical('Scheduler not found, exiting')
+            sys.exit(1)
+        logging.info('Starting downloader, with %s threads', len(self._threads))
+        for t in self._threads:
+            t.start()
+
+    def _init_threads(self, thread_count):
         for i in xrange(thread_count):
             t = threading.Thread(target=self._download)
             t.daemon = True
             self._threads.append(t)
-
-    def set_handler(self, hdlr):
-        self._item_handler = hdlr
 
     def _download(self):
         logging.info('Downloader thread %s started', threading.current_thread().name)
@@ -36,30 +57,18 @@ class Downloader(object):
                 item = self._in_queue.get()
                 logging.debug('Downloader thread %s start downloading: %s', threading.current_thread().name, item.url)
                 self._do_download(item)
-            except requests.HTTPError as error:
+            except requests.RequestException as error:
                 logging.error(error.message)
             finally:
                 self._item_handler.add_item(item)
 
-
     def _do_download(self, item):
         logging.debug('Item %s start downloading', item.url)
-        r = requests.get(item.url)
+        r = requests.get(item.url, timeout=self._crawl_timeout)
         item.final_url = r.url
         r.raise_for_status()
-        logging.debug('Item %s downloaded', item.url)
+        logging.debug('Item %s downloaded', item.final_url)
         item.headers = r.headers
         item.encoding = r.apparent_encoding
         item.content = r.content
         item.is_requested = True
-
-    def start(self):
-        logging.info('Starting downloader, with %s threads', len(self._threads))
-        for t in self._threads:
-            t.start()
-
-    def stop(self):
-        pass
-
-    def add_item(self, item):
-        self._in_queue.put(item)
--- item.py
+++ item.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 class Item(object):
+    """
+    下载项目
+    在初始化和解析链接后创建于Scheduler，随后沿着Scheduler->Downloader->ItemHandler->Scheduler路径流动
+    """
     def __init__(self, url, TTL):
         self.url = url
         self.TTL = TTL
